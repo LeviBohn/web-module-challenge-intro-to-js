70eae06ead0161913810690262786ba47241036c
Levi Bohn <levibohn@gmail.com>
2022-10-06 16:16:26 -0500

    sprint 2 module 2 project complete

--- index.js
+++ index.js
@@ -283,11 +283,11 @@ function grade(number){
 if (number >= 90){
   return 'you got an A';
 } else if (number >= 80){
-  return 'you got an B';
+  return 'you got a B';
 } else if (number >= 70){
-  return 'you got an C';
+  return 'you got a C';
 } else if (number >= 60){
-  return 'you got an D';
+  return 'you got a D';
 } else {
   return 'you got an F';
 }
